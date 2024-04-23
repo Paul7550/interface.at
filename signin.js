66c0d45f25a3b112677afe84c2257bc6813b93b6
paul albrecht <paul.albrecht@it.htl-hl.ac.at>
2024-04-23 21:31:17 +0200

    safing usernames work

--- signin.js
+++ signin.js
@@ -1,21 +1,51 @@
 const username = [];
 const password = [];
+const email = [];
 let usercount = 0;
 var safeusername;
 function signin() {
+  usercount = getCookie("usercount");
+  if (!usercount) {
+    usercount = 0;
+  }
   for (i = 0; i <= usercount; i++) {
-    if (
-      document.getElementById("username") != username[i] &&
-      document.getElementById("cfmpassword") ==
-        document.getElementById("password")
-    ) {
-      username[usercount] = document.getElementById("username");
-      password[usercount] = document.getElementById("passwort");
+    if (document.getElementById("email").value == email[i]) {
+      document.getElementById("").innerHTML =
+        "diese Email wurde bereits verwendet"; //Name für bereits benutzter email
+      return;
     }
   }
+
+  if (
+    document.getElementById("cfmpassword").value ==
+    document.getElementById("password").value
+  ) {
+    usercount++;
+    email[usercount] = document.getElementById("email");
+    username[usercount] = document.getElementById("username").value;
+    password[usercount] = document.getElementById("password").value;
+    setCookie("usercount", usercount);
+    setCookie("logdin", usercount);
+    //document.getElementById("link").href = "";
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+  } else {
+    document.getElementById("").innerHTML = //hier namen für nicht gleiches passwort
+      "Die Passwörter stimmen nicht überein";
+  }
+  document.getElementById("cfmpassword").value = "";
+  document.getElementById("password").value = "";
   store();
 }
-function store() {}
+function store() {
+  XMLDocument;
+  if (safeusername == undefined) {
+    safeusername = String(username.join() + ",");
+  } else {
+    safeusername += String(username.join() + ",");
+  }
+  setCookie("usernames", safeusername);
+}
 function setCookie(cname, cvalue) {
   document.cookie = cname + "=" + cvalue + ";";
 }
