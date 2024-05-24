3741a43f5047e8ba86215cec69c43d8320e65026
paul albrecht <paul.albrecht@it.htl-hl.ac.at>
2024-05-24 07:51:31 +0200

    function friends

--- js/index.js
+++ js/index.js
@@ -2,84 +2,91 @@ var logdin = getCookie("logdinuser");
 let followedlistuser = [];
 let userx = logdin - 1;
 let count;
+let imgurl = JSON.parse(getCookie("imglist"));
 followedlistuser = JSON.parse(getCookie(`followedlist${userx}`));
 function load() {
-  if (logdin != "") {
-    document.getElementById("logout").hidden = false;
-    document.getElementById("login").hidden = true;
-    document.getElementById("userx").hidden = false;
-  } else {
-    document.getElementById("logout").hidden = true;
-    document.getElementById("login").hidden = false;
-    document.getElementById("userx").hidden = true;
-  }
-  if (followedlistuser.length > 0) {
-    document.getElementById("noFriends").hidden = true;
-  }
+    if (logdin != "") {
+        document.getElementById("logout").hidden = false;
+        document.getElementById("login").hidden = true;
+        document.getElementById("userx").hidden = false;
+        friends();
+    } else {
+        document.getElementById("logout").hidden = true;
+        document.getElementById("login").hidden = false;
+        document.getElementById("userx").hidden = true;
+    }
 }
 function friends() {
-  for (i = 0; i < followedlistuser.length; i++) {
-    const link = document.createElement("a");
-    const img = document.createElement("img");
-    link.id = "link";
-    img.classList.add("profilepic");
-    link.appendChild(img);
-    link.addEventListener("click", (event) => {
-      userX(followedlistuser[i]);
-    });
-    document.getElementById("profilenavbar").appendChild(link);
-    document.getElementById("link").href = "userx.html";
-  }
+    if (followedlistuser.length > 0) {
+        document.getElementById("profilenavbar").innerHTML = "";
+        for (i = 0; i < followedlistuser.length; i++) {
+            const profilepic = document.createElement("div");
+            const img = document.createElement("img");
+            profilepic.classList.add("profilepic");
+            let countX = i;
+            img.id = `img${i}`;
+            profilepic.appendChild(img);
+            profilepic.addEventListener("click", (event) => {
+                userX(countX);
+            });
+            document.getElementById("profilenavbar").appendChild(profilepic);
+            if (imgurl[userx] != undefined) {
+                document.getElementById(`img${i}`).src = imgurl[followedlistuser[i]];
+            } else {
+                document.getElementById(`img${i}`).src = "../sources/person_24dp_FILL0_wght400_GRAD0_opsz24.png";
+            }
+        }
+    }
 }
 function userX(greeting) {
-  setCookie("userX", greeting - 1);
+    setCookie("userX", greeting - 1);
 }
 function profile() {
-  setCookie("userX", logdin - 1);
+    setCookie("userX", logdin - 1);
 }
 function logout() {
-  logdin = "";
-  document.getElementById("logout").hidden = true;
-  setCookie("logdinuser", logdin);
-  load();
+    logdin = "";
+    document.getElementById("logout").hidden = true;
+    setCookie("logdinuser", logdin);
+    load();
 }
 function post() {
-  const main = document.createElement("div");
-  main.classList.add("post");
-  main.classList.add("col-sm-12");
-  const profilepicture = document.createElement("div");
-  profilepicture.classList.add("profilepicture");
-  const image = document.createElement("img");
-  image.classList.add("image");
-  const username = document.createElement("div");
-  username.classList.add("username");
-  const date = document.createElement("div");
-  date.classList.add("date");
-  const description = document.createElement("div");
-  description.classList.add("description");
-  profilepicture.appendChild(image);
-  main.appendChild(profilepicture);
-  username.appendChild(date);
-  main.appendChild(username);
-  main.appendChild(description);
-  document.getElementById("posts").appendChild(main);
+    const main = document.createElement("div");
+    main.classList.add("post");
+    main.classList.add("col-sm-12");
+    const profilepicture = document.createElement("div");
+    profilepicture.classList.add("profilepicture");
+    const image = document.createElement("img");
+    image.classList.add("image");
+    const username = document.createElement("div");
+    username.classList.add("username");
+    const date = document.createElement("div");
+    date.classList.add("date");
+    const description = document.createElement("div");
+    description.classList.add("description");
+    profilepicture.appendChild(image);
+    main.appendChild(profilepicture);
+    username.appendChild(date);
+    main.appendChild(username);
+    main.appendChild(description);
+    document.getElementById("posts").appendChild(main);
 }
 
 function setCookie(cname, cvalue) {
-  document.cookie = cname + "=" + cvalue + ";";
+    document.cookie = cname + "=" + cvalue + ";";
 }
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
--- js/userx.js
+++ js/userx.js
@@ -10,101 +10,99 @@ let followerlistx = [];
 let followedlistuser = [];
 var logdin = getCookie("logdinuser");
 function load() {
-  userx = getCookie("userX");
-  followedlistuser = JSON.parse(getCookie(`followedlist${userx}`));
-  followerlistx = JSON.parse(getCookie(`followerlist${userx}`));
-  username = JSON.parse(getCookie("usernames"));
-  email = JSON.parse(getCookie("emails"));
-  usercount = parseInt(getCookie("usercount"));
-  password = JSON.parse(getCookie("passwords"));
-  usercolor = JSON.parse(getCookie("usercolor"));
-  imgurl = JSON.parse(getCookie("imglist"));
-  userdes = JSON.parse(getCookie("userdes"));
-  userx++;
-  if (userx == logdin) {
-    document.getElementById("editprofilebutton").hidden = false;
-    document.getElementById("contact").hidden = true;
-  }
-  userx--;
-  show();
+    userx = getCookie("userX");
+    followedlistuser = JSON.parse(getCookie(`followedlist${userx}`));
+    followerlistx = JSON.parse(getCookie(`followerlist${userx}`));
+    username = JSON.parse(getCookie("usernames"));
+    email = JSON.parse(getCookie("emails"));
+    usercount = parseInt(getCookie("usercount"));
+    password = JSON.parse(getCookie("passwords"));
+    usercolor = JSON.parse(getCookie("usercolor"));
+    imgurl = JSON.parse(getCookie("imglist"));
+    userdes = JSON.parse(getCookie("userdes"));
+    userx++;
+    if (userx == logdin) {
+        document.getElementById("editprofilebutton").hidden = false;
+        document.getElementById("contact").hidden = true;
+    }
+    userx--;
+    show();
 }
 function edit() {
-  document.getElementById("editdes").hidden = false;
-  document.getElementById("backroundcolor").hidden = false;
-  document.getElementById("url").hidden = false;
+    document.getElementById("editdes").hidden = false;
+    document.getElementById("backroundcolor").hidden = false;
+    document.getElementById("url").hidden = false;
 }
 function img() {
-  imgurl[userx] = document.getElementById("imgurl").value;
-  let saveimg = JSON.stringify(imgurl);
-  setCookie("imglist", saveimg);
-  document.getElementById("imgurl").value = "";
-  document.getElementById("url").hidden = true;
-  show();
+    imgurl[userx] = document.getElementById("imgurl").value;
+    let saveimg = JSON.stringify(imgurl);
+    setCookie("imglist", saveimg);
+    document.getElementById("imgurl").value = "";
+    document.getElementById("url").hidden = true;
+    show();
 }
 function clearimg() {
-  imgurl[userx] = undefined;
-  let saveimg = JSON.stringify(imgurl);
-  setCookie("imglist", saveimg);
-  document.getElementById("url").hidden = true;
-  show();
+    imgurl[userx] = undefined;
+    let saveimg = JSON.stringify(imgurl);
+    setCookie("imglist", saveimg);
+    document.getElementById("url").hidden = true;
+    show();
 }
 function follow() {
-  followerlistx[followerlistx.length] = userx;
-  let safefollowerlistx = JSON.stringify(followerlistx);
-  setCookie(`followerlist${userx}`, safefollowerlistx);
-  followedlistuser[followedlistuser.length] = userx;
-  let safefollowerlistuser = JSON.stringify(followedlistuser);
-  setCookie(`followedlist${logdin - 1}`, safefollowerlistuser);
+    followerlistx[followerlistx.length] = userx;
+    let safefollowerlistx = JSON.stringify(followerlistx);
+    setCookie(`followerlist${userx}`, safefollowerlistx);
+    followedlistuser[followedlistuser.length] = userx;
+    let safefollowerlistuser = JSON.stringify(followedlistuser);
+    setCookie(`followedlist${logdin - 1}`, safefollowerlistuser);
 }
 function savecolor() {
-  usercolor[userx] = document.getElementById("backround").value;
-  let safeusercolor = JSON.stringify(usercolor);
-  setCookie("usercolor", safeusercolor);
-  document.getElementById("backroundcolor").hidden = true;
-  document.getElementById("profile").style.backgroundColor = usercolor[userx];
-  show();
+    usercolor[userx] = document.getElementById("backround").value;
+    let safeusercolor = JSON.stringify(usercolor);
+    setCookie("usercolor", safeusercolor);
+    document.getElementById("backroundcolor").hidden = true;
+    document.getElementById("profile").style.backgroundColor = usercolor[userx];
+    show();
 }
 function show() {
-  document.getElementById("profile").style.backgroundColor = usercolor[userx];
-  document.getElementById("username").innerHTML = username[userx];
-  if (userdes[userx] == undefined) {
-    document.getElementById("des").innerHTML = "no Bio yet";
-  } else {
-    document.getElementById("des").innerHTML = userdes[userx];
-  }
-  if (imgurl[userx] != undefined) {
-    document.getElementById("profileimg").src = imgurl[userx];
-  } else {
-    document.getElementById("profileimg").src =
-      "../sources/person_24dp_FILL0_wght400_GRAD0_opsz24.png";
-  }
-  document.getElementById("followercounter").innerHTML = followerlistx.length;
-  document.getElementById("followedcounter").innerHTML =
-    followedlistuser.length;
+    document.getElementById("profile").style.backgroundColor = usercolor[userx];
+    document.getElementById("username").innerHTML = username[userx];
+    if (userdes[userx] == undefined) {
+        document.getElementById("des").innerHTML = "no Bio yet";
+    } else {
+        document.getElementById("des").innerHTML = userdes[userx];
+    }
+    if (imgurl[userx] != undefined) {
+        document.getElementById("profileimg").src = imgurl[userx];
+    } else {
+        document.getElementById("profileimg").src = "../sources/person_24dp_FILL0_wght400_GRAD0_opsz24.png";
+    }
+    document.getElementById("followercounter").innerHTML = followerlistx.length;
+    document.getElementById("followedcounter").innerHTML = followedlistuser.length;
 }
 
 function savedes() {
-  userdes[userx] = document.getElementById("inputdes").value;
-  let safeuserdes = JSON.stringify(userdes);
-  setCookie("userdes", safeuserdes);
-  document.getElementById("editdes").hidden = true;
-  show();
+    userdes[userx] = document.getElementById("inputdes").value;
+    let safeuserdes = JSON.stringify(userdes);
+    setCookie("userdes", safeuserdes);
+    document.getElementById("editdes").hidden = true;
+    show();
 }
 function setCookie(cname, cvalue) {
-  document.cookie = cname + "=" + cvalue + ";";
+    document.cookie = cname + "=" + cvalue + ";";
 }
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
